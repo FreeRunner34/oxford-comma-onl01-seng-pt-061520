5140f2fce48008c6e70005811e2cf359d3df7060
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-06-24 05:05:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,16 +3,3 @@
 #  array.join(" ")
 #end
 
-
-def oxford_comma(array)
-
-end 
-  case array.length 
-when 1
-  "#{array[0]}"
-when 2
-  array[0..1].join(" and ")
-else 
-  array[0...-1].join(", ") << ", and #{array[-1]}"
-end 
-end
